e36593d51a2bb419d4be824630f548baa3270d25
Josue <josue_noob@hotmail.com>
2024-10-18 13:43:50 -0600

    ajuste config

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app'); // Asegúrate de que la ruta es correcta
-const port = process.env.PORT || 3700;
 
 // Cargar variables de entorno desde el archivo .env
 dotenv.config();
@@ -11,9 +10,11 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 .then(() => {
     console.log("Conexión a la base de datos establecida con éxito...");
     
-    // Crear el servidor
-    app.listen(port, () => {
-        console.log("Servidor corriendo correctamente en la url" + port);
-    });
+    if (process.env.NODE_ENV !== 'test') {
+        // Solo iniciar el servidor si no estamos en modo de prueba
+        app.listen(process.env.PORT || 3700, () => {
+            console.log(`Servidor corriendo correctamente en la url: localhost:${process.env.PORT || 3700}`);
+        });
+    }
 })
-.catch(err => console.log(err));
+.catch(err => console.error('No se pudo conectar a MongoDB Atlas:', err));
