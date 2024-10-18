f3d2fec2585bddeccb758437bde84542c0e81d39
Josue <josue_noob@hotmail.com>
2024-10-18 14:51:00 -0600

    prueba

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,18 +1,15 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const serverless = require('serverless-http');
-const app = require('./app'); // Asegúrate de que la ruta es correcta
+const app = require('./app');
 
-// Cargar variables de entorno desde el archivo .env
 dotenv.config();
 
 mongoose.Promise = global.Promise;
-
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {
     console.log("Conexión a la base de datos establecida con éxito...");
 })
 .catch(err => console.error('No se pudo conectar a MongoDB Atlas:', err));
 
-// Exportar la aplicación con serverless
 module.exports.handler = serverless(app);
