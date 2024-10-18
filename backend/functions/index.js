85628eb6f1929bbcb0fbbb49d9ea0cad8d0a4193
Josue <josue_noob@hotmail.com>
2024-10-18 12:31:59 -0600

    Usar variables de entorno para la conexión a MongoDB Atlas

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,15 +1,19 @@
 const mongoose = require('mongoose');
-const app = require('./app');
+const dotenv = require('dotenv');
+const app = require('.app'); // Asegúrate de que la ruta es correcta
 const port = process.env.PORT || 3700;
 
+// Cargar variables de entorno desde el archivo .env
+dotenv.config();
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/portafolio')
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {
     console.log("Conexión a la base de datos establecida con éxito...");
     
     // Crear el servidor
     app.listen(port, () => {
-        console.log("Servidor corriendo correctamente en la url: localhost:" + port);
+        console.log("Servidor corriendo correctamente en la url" + port);
     });
 })
 .catch(err => console.log(err));
