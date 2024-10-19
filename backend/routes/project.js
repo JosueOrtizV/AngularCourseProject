53e17cdf284cc091a603c99d070085dad042c005
Josue <josue_noob@hotmail.com>
2024-10-18 22:43:39 -0600

    controller

--- backend/routes/project.js
+++ backend/routes/project.js
@@ -1,15 +1,15 @@
 import express from 'express';
-import ProjectController from '../controllers/project.js';
+import controller from '../controllers/project.js'; 
 const router = express.Router();
 
-router.get('/home', ProjectController.home);
-router.post('/test', ProjectController.test);
-router.post('/save-project', ProjectController.saveProject);
-router.get('/project/:id?', ProjectController.getProject);
-router.get('/projects', ProjectController.getProjects);
-router.put('/project/:id', ProjectController.updateProject);
-router.delete('/project/:id', ProjectController.deleteProject);
-router.post('/upload-image/:id', ProjectController.uploadImage);
-router.get('/get-image/:image', ProjectController.getImageFile);
+router.get('/home', controller.home);
+router.post('/test', controller.test);
+router.post('/save-project', controller.saveProject);
+router.get('/project/:id?', controller.getProject);
+router.get('/projects', controller.getProjects);
+router.put('/project/:id', controller.updateProject);
+router.delete('/project/:id', controller.deleteProject);
+router.post('/upload-image/:id', controller.uploadImage);
+router.get('/get-image/:image', controller.getImageFile);
 
 export default router;
