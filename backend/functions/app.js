c6635b5726af063afba208d2055c6d17b06aa125
Josue <josue_noob@hotmail.com>
2024-10-18 22:29:05 -0600

    serverless

--- backend/functions/app.js
+++ backend/functions/app.js
@@ -1,22 +1,26 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import path from 'path';
-import fs from 'fs';
 import multer from 'multer';
 import projectRoutes from '../routes/project.js';
 
+// Inicializa Express
 const app = express();
 
+// Middleware para el parsing de datos
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
+// Configuración de multer para la subida de imágenes en memoria
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-app.use(upload.single('image')); // Middleware de multer
+// Usa el middleware de multer para gestionar las cargas de archivos (imágenes)
+app.use(upload.single('image'));
 
+// Rutas
 app.use('/api', projectRoutes);
 
+// Exporta la aplicación express (será utilizado en serverless-http en index.js)
 export default app;
--- backend/functions/index.js
+++ backend/functions/index.js
@@ -5,12 +5,26 @@ import app from './app.js';
 
 dotenv.config();
 
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => {
-    console.log("Conexión a la base de datos establecida con éxito...");
-})
-.catch(err => console.error('No se pudo conectar a MongoDB Atlas:', err));
+let isConnected;
 
-const handler = serverless(app);
+const connectToDatabase = async () => {
+    if (isConnected) {
+        console.log('Usando conexión existente a la base de datos.');
+        return;
+    }
+    
+    console.log('Creando nueva conexión a la base de datos...');
+    await mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    isConnected = mongoose.connection.readyState;
+};
+
+const handler = async (event, context) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    await connectToDatabase();
+    return serverless(app)(event, context);
+};
+
 export { handler };
